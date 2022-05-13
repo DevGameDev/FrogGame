2f301b04067e374418f68dbafd9cc040c326274d
Devin <devinhenderling@gmail.com>
2022-05-13 13:37:59 -0700

    Unit class progress + FrogBasic
    + slight .gitignore update

--- FrogGame-UnityProject/Assets/Scripts/Unit.cs
+++ FrogGame-UnityProject/Assets/Scripts/Unit.cs
@@ -2,17 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public interface IDamageable
+{
+    static int startHealth;
+    int currentHealth { get; set; }
+    void ApplyDamage(int damage);
+}
+
+public interface IDamageDealer
+{
+    static int startAttack;
+    int currentAttack { get; set; }
+    void DealDamage(int damage);
+}
+
+public interface IHealer
+{
+    int startHealing { get; set; };
+    int currentHealing { get; set; }
+    void Heal(int healing);
+}
+
 public class Unit : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Basic Unit Info
+    string name;
+
     void Start()
     {
-        
+        Debug.Log($"Start - Unit: {name}")
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        UpdateInfo();
+    }
+
+    void UpdateInfo() 
+    {
+        return;
     }
 }
