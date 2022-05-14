0fedcf1bd67f17a05e601b54df3abb4959b3b11e
Peter N <singulus101@protonmail.com>
2022-05-14 13:51:19 -0700

    reimported script changes
    edited art-test scene to add mouse control functions, strange camera overlay bug

---
+++ FrogGame-UnityProject/Assets/Scripts/MouseControls.cs
@@ -0,0 +1,45 @@
+using System.Collections;  
+using System.Collections;  
+using UnityEngine;  
+  
+public class MouseControls: MonoBehaviour {  
+  
+  private Renderer renderer;
+  public GameObject[] pads;
+  public GameObject selectedPad;
+  void Start() 
+  {
+    pads = GameObject.FindGameObjectsWithTag("lilypad");
+  }  
+  
+    
+  void Update() 
+  {  
+    if (Input.GetMouseButtonDown(0)) {  
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);  
+      RaycastHit hit;  
+      if (Physics.Raycast(ray, out hit)) {  
+           
+        if (hit.transform.tag == "tile") {  
+          foreach (GameObject pad in pads) {
+            padColor padcolor = pad.GetComponent<padColor>();
+            padcolor.isSelected = false;
+          }
+          //selectedPad = hit.transform.FindGameObjectsWithTag("lilypad");
+          if (hit.transform.childCount > 0)
+                {
+                    // Add tile to list if it contains a child with the tag
+                    foreach (Transform child in hit.transform)
+                    {
+                        if (child.gameObject.tag == "lilypad")
+                        {
+                          padColor padcolor = child.GetComponent<padColor>();
+                          padcolor.isSelected = true;
+                        }
+                    }
+                } 
+        } 
+      }  
+    }  
+  }  
+} 
